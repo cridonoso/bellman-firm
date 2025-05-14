a7237143025cae67b687a10f89b65e594e360ed6
Cristobal Donoso <cridonoso@inf.udec.cl>
2025-05-13 23:27:27 -0400

    bellman 2 void

--- utils/bellman_2.m
+++ utils/bellman_2.m
@@ -1,7 +1,5 @@
-function [outputArg1,outputArg2] = bellman_1a(inputArg1,inputArg2)
+function [] = bellman_2()
 %BELLMAN_1A Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
 end
 
